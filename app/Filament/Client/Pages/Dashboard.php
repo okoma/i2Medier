7a3d2016a036d09bb8655d0290545872a19006d9
Okoma <okoma078@gmail.com>
2026-06-10 12:09:07 +0100

    feat(portal): convert Dashboard to widget grid, remove blade template

--- app/Filament/Client/Pages/Dashboard.php
+++ app/Filament/Client/Pages/Dashboard.php
@@ -2,11 +2,25 @@
 
 namespace App\Filament\Client\Pages;
 
+use App\Filament\Client\Widgets\Dashboard\DashboardStats;
+use App\Filament\Client\Widgets\Dashboard\RecentInvoicesWidget;
+use App\Filament\Client\Widgets\Dashboard\RecentProjectsWidget;
+use App\Filament\Client\Widgets\Dashboard\RecentTicketsWidget;
+use App\Filament\Client\Widgets\UpcomingRenewals;
 use Filament\Pages\Dashboard as BaseDashboard;
 
 class Dashboard extends BaseDashboard
 {
-    protected string $view = 'filament.client.pages.dashboard';
+    protected function getWidgets(): array
+    {
+        return [
+            DashboardStats::class,
+            RecentProjectsWidget::class,
+            UpcomingRenewals::class,
+            RecentInvoicesWidget::class,
+            RecentTicketsWidget::class,
+        ];
+    }
 
     protected function getHeaderWidgets(): array
     {
